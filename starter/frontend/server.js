df2138b09b19e7d6ab42e7fc2fd6581beae77373
falbellaihi1 <falbella@mail.umw.edu>
2020-09-23 14:12:22 +0300

    updated server.js

--- starter/frontend/server.js
+++ starter/frontend/server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-    console.log('port', app.get('port'));
-});
+app.listen(process.env.PORT || 5000)
 
 
