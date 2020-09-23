d7d700fafd4baa7f81dd03b53715abc0ad5cb03a
falbellaihi1 <falbella@mail.umw.edu>
2020-09-23 13:58:32 +0300

    fixed typeos in serve.js

--- starter/frontend/server.js
+++ starter/frontend/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
-var app = express()
-app.use(express.static('www'));
+var app = express();
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('port', app.get('port'));
