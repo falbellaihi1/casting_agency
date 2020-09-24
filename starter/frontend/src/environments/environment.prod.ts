362c603f5d489d745b6d4672f3630072323fff35
falbellaihi1 <falbella@mail.umw.edu>
2020-09-24 13:32:33 +0300

    updated environment.ts

--- starter/frontend/src/environments/environment.prod.ts
+++ starter/frontend/src/environments/environment.prod.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: true,
-  apiServerUrl: 'https://casting-agency-capstone-udaciy.herokuapp.com/', // the running FLASK api server url
+  apiServerUrl: 'https://casting-agency-capstone-udaciy.herokuapp.com', // the running FLASK api server url
   auth0: {
     url: 'falbellaihi1.us', // the auth0 domain prefix
     audience: 'castingagency', // the audience set for the auth0 app
     clientId: 'pEvos3o5ZG6MTiTSQaT12hDNOEsGJIjR', // the client id generated for the auth0 app
-    callbackURL: 'https://casting-agency-falbellaihi.herokuapp.com/', // the base url of the running ionic application.
+    callbackURL: 'https://casting-agency-falbellaihi.herokuapp.com', // the base url of the running ionic application.
   }
 };
