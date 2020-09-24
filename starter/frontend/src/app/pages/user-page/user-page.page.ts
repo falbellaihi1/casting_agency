3e4ed9d1fe436cb74da609999ad53a9a60836164
falbellaihi1 <falbella@mail.umw.edu>
2020-09-24 14:12:28 +0300

    updaed callback url

--- starter/frontend/src/app/pages/user-page/user-page.page.ts
+++ starter/frontend/src/app/pages/user-page/user-page.page.ts
@@ -11,7 +11,7 @@ export class UserPagePage implements OnInit {
   logoutURL: string;
 
   constructor(public auth: AuthService) {
-    this.loginURL = auth.build_login_link('tabs/user-page');
+    this.loginURL = auth.build_login_link('user-page');
     this.logoutURL = auth.build_logout_link();
 
   }
